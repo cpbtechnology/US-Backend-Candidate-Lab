d9fcc2e9de66e70603e2a8b184f32d3d02a9291d
Luis Gonzalez <luis.gonzalez@rockfishinteractive.com>
2013-10-04 17:32:49 -0500

    Notes Add Test Completed

--- NotesApi/App_Start/WebApiConfig.cs
+++ NotesApi/App_Start/WebApiConfig.cs
@@ -8,7 +8,7 @@ namespace NotesApi
 		{
 			config.Routes.MapHttpRoute(
 				name: "DefaultApi",
-				routeTemplate: "api/{controller}/{id}",
+				routeTemplate: "api/{controller}/{action}/{id}",
 				defaults: new { id = RouteParameter.Optional }
 			);
 
--- NotesApiWeb/Scripts/custom.js
+++ NotesApiWeb/Scripts/custom.js
@@ -1,4 +1,5 @@
-﻿function addNote() {
+﻿
+function addNote() {
 	var wWidth = $(window).width();
 	console.log(wWidth);
 	var dWidth = wWidth * 0.5;
@@ -6,4 +7,19 @@
 		width: dWidth,
 		modal: true
 	});
+}
+
+
+function submitForm(frm) {
+	if (!$(frm).valid()) { return false; }
+	console.log($(frm).serialize());
+	$.post($(frm).attr("action"), $(frm).serialize(), function (data) {
+		
+		console.log(data);
+	});
+	return false;
+}
+
+function cancelForm() {
+	
 }
