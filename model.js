84ec90d750b8da7fac94be664487aaed8e4463c8
Pavel <d4rklit3@gmail.com>
2013-11-06 00:12:30 -0800

    created auth validation

--- model.js
+++ model.js
@@ -1,5 +1,5 @@
 
-//begin notesModel
+//begin notesModels
 
 
 
@@ -9,22 +9,28 @@ module.exports = (function () {
 
     var mongoose = require('mongoose'),
         autoIncrement = require('mongoose-auto-increment'),
-        db = mongoose.connection;
+        db = mongoose.connection,
+        crypto = require("crypto"),
+        restify = require("restify");
+
+
 
 
 
     var Note = null;
+    var User = null;
     var _this = null;
     
    
-    var notesModel = function () {
-        _this = notesModel;
+    var notesModels = function () {
+        _this = notesModels;
         return _this;
     };
 
     var createSchema = function (c) {
 
 
+        //Note
         var noteSchema = mongoose.Schema({
             title: String
             ,body: String
@@ -32,27 +38,111 @@ module.exports = (function () {
             ,createdDate: Date
         });
 
+        //User
+        var userSchema = mongoose.Schema({
+            email:String
+            ,username: String
+            ,password: String
+            ,createdDate: Date
+            
+        });
 
+        userSchema.methods.validatePassword = function (password) {
+            var hash = crypto.createHash('md5').update(password+this.createdDate.toISOString()).digest('hex');
+            if(hash === this.password)
+                return true;
+            else
+                return false;
+        }
 
+        //init auto increment plugin
         autoIncrement.initialize(c);
         noteSchema.plugin(autoIncrement.plugin, "Note");
-        Note = mongoose.model("Note" , noteSchema);
+        userSchema.plugin(autoIncrement.plugin, "User");
 
+        //declare models
+        Note = mongoose.model("Note" , noteSchema);      
+        User = mongoose.model("User" , userSchema);
 
     };
 
 
+    notesModels.createUser = function (user , callback) {
+        
+        //ToDo
+        // Salt with DOB
 
-    notesModel.createNote = function (data, user, callback) { 
+
+        var user = user;
+        _this.getUser(user.username , function (err, userExists) {
+            if(userExists)
+                callback(new restify.InvalidArgumentError("Username Exists"));
+            else {
+                var now = new Date().toISOString();
+    
+                
+                user.password = hash;
+                var newUser = new User(user);
+                newUser.createdDate = now;
+                newUser.save(callback);
+            }
+        });
+
+        
+
+
+
+    }
+
+    notesModels.authUser = function (userCred , callback) {
+
+        
+        
+        _this.getUser(userCred.username , function (err, user) {
+            if(err) {
+                callback(new restify.InternalError(err));
+            }else if(user) {                
+                if(user.validatePassword(userCred.password)) {
+                    callback(null, user);
+                }
+                else{
+                    callback(new restify.NotAuthorizedError("Access Denied"));
+                }
+
+            }else{
+                callback(new restify.ResourceNotFoundError("User Does Not Exist"))
+            }  
+        });
+
+
+    }
+
+    notesModels.getUser = function (username, callback) { 
+        User.findOne({username:username}).lean().exec(function (err, user) {
+            if(err)
+                callback("User Does Not Exist");
+            else if(user)
+                callback(null, user)
+            else
+                callback("User Does Not Exist");
+
+        });
+
+    }
+
+
+
+    notesModels.createNote = function (data, user, callback) { 
 
     
         var note = new Note(data);
+        note.createdDate = new Date().toISOString();
         note.save(callback);       
 
 
     };
 
-    notesModel.getNote = function (id, user, callback) { 
+    notesModels.getNote = function (id, user, callback) { 
 
 
         if(id !== undefined) 
@@ -62,19 +152,19 @@ module.exports = (function () {
 
     };
 
-    notesModel.updateNote = function(id , data, user, callback) { 
+    notesModels.updateNote = function(id , data, user, callback) { 
        
         Note.update({ _id: id} , data, callback);
 
     };
 
-    notesModel.delNote = function(id, user, callback) {
+    notesModels.delNote = function(id, user, callback) {
 
         Note.remove({_id:id} , callback);
     };
 
 
-    notesModel.connect = function (host, options) {
+    notesModels.connect = function (host, options) {
         db.on('error' , function () {
             console.log("Something Went Wrong", arguments);
         });
@@ -93,7 +183,7 @@ module.exports = (function () {
 
 
 
-    return notesModel;
+    return notesModels;
 
 
 
--- server.js
+++ server.js
@@ -16,6 +16,10 @@ var notesApi = (function () {
     var pkg = require('./package.json');
     var notesModel = require("./model.js");
     var model = new notesModel();
+    var passport = require('passport')
+        ,LocalStrategy = require('passport-local').Strategy;
+
+
 
 
     //Context Helper
@@ -36,19 +40,32 @@ var notesApi = (function () {
 
         return _this;
     };  
+
+
+    var initAuth = function () {
+        passport.use(new LocalStrategy(
+            function(username, password, done) {
+               
+            }
+        ));
+    }
     
 
     notesApi.start = function() {
+        
         model.connect("mongodb://localhost/notes");
+        initAuth();
         _this.server.listen(8080 , function() {
             console.log(pkg.name + " online.");
         });
-        _this.server.use(restify.authorizationParser());
+        //_this.server.use(restify.authorizationParser());
         _this.server.use(restify.dateParser());
         _this.server.use(restify.queryParser());
         _this.server.use(restify.jsonp());
         _this.server.use(restify.gzipResponse());
         _this.server.use(restify.bodyParser());
+        _this.server.use(passport.initialize());
+        _this.server.use(passport.session());
         _this.setupRoutes();
     };
 
@@ -56,13 +73,51 @@ var notesApi = (function () {
     notesApi.setupRoutes = function () {
 
         var server = _this.server;
+
+        server.put("/api/user" , _this.actionCreateUser);
+        server.post("/api/user/login" , _this.actionLogin);
         server.get("/api/notes" , _this.actionGet);
         server.get("/api/notes/:id" , _this.actionGet);
         server.put("/api/notes" , _this.actionCreate);
         server.post("/api/notes/:id" , _this.actionUpdate);
         server.del("/api/notes/:id" , _this.actionDelete);
+        
 
     };
+
+    //User Crud
+    notesApi.actionCreateUser = function (req, res, next) {
+        try {
+            model.createUser(JSON.parse(req.body), function (err, data) {
+                if(err){
+                    return next(err);
+                }else{
+                    console.log("send");
+                    res.send(201);
+
+                }
+                    
+            });  
+         } catch(err){
+            return next(err);
+        }
+        
+    };
+
+    notesApi.actionLogin = function (req, res, next) {
+        try {
+            model.authUser(JSON.parse(req.body), function (err, data) {
+                if(err)
+                    return next(err);
+                else
+                    res.send(200);
+            });  
+         } catch(err){
+            return next(err);
+        }
+        
+    };
+
 
     //Notes Crud
     notesApi.actionGet = function(req, res, next) { 
@@ -70,7 +125,7 @@ var notesApi = (function () {
         try {
             model.getNote(req.params.id, null, function (err, data) {
                 if(err)
-                    next(err)
+                    return next(err);
                 else
                     res.send(200, data);
             });  
@@ -84,7 +139,7 @@ var notesApi = (function () {
         try {
             model.createNote(JSON.parse(req.body), null , function (err, data) {
                 if(err)
-                    next(err)
+                    return next(err);
                 else
                     res.send(201);
             });            
@@ -98,7 +153,7 @@ var notesApi = (function () {
         try {
             model.updateNote(req.params.id , JSON.parse(req.body) ,null , function (err, updRows ,data) {
                 if(err)
-                    next(err)
+                    return next(err);
                 else
                     res.send(204);
             });            
@@ -111,7 +166,7 @@ var notesApi = (function () {
         try {
             model.delNote(req.params.id ,null , function (err,  data) {
                 if(err)
-                    next(err)
+                    next(err);
                 else
                     res.send(204);
             });            
