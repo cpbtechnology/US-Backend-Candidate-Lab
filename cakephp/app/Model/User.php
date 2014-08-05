363a57d04159cc5747d26b81eb372680ac590d11
evan.spielberg@gmail.com <evan.spielberg@gmail.com>
2014-08-05 19:29:02 -0400

    Create User Model
    Use Cake Bake

---
+++ cakephp/app/Model/User.php
@@ -0,0 +1,41 @@
+<?php
+App::uses('AppModel', 'Model');
+/**
+ * User Model
+ *
+ * @property Note $Note
+ */
+class User extends AppModel {
+
+/**
+ * Display field
+ *
+ * @var string
+ */
+	public $displayField = 'username';
+
+
+	//The Associations below have been created with all possible keys, those that are not needed can be removed
+
+/**
+ * hasMany associations
+ *
+ * @var array
+ */
+	public $hasMany = array(
+		'Note' => array(
+			'className' => 'Note',
+			'foreignKey' => 'user_id',
+			'dependent' => false,
+			'conditions' => '',
+			'fields' => '',
+			'order' => '',
+			'limit' => '',
+			'offset' => '',
+			'exclusive' => '',
+			'finderQuery' => '',
+			'counterQuery' => ''
+		)
+	);
+
+}
