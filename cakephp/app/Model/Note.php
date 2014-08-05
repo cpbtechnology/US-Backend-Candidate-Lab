68b40a497c002c6905cf71dc86e2521e77dd71a9
evan.spielberg@gmail.com <evan.spielberg@gmail.com>
2014-08-05 19:10:09 -0400

    Create the Note Model using Cake Bake
    CakePHP can automatically create models based on the database.

---
+++ cakephp/app/Model/Note.php
@@ -0,0 +1,34 @@
+<?php
+App::uses('AppModel', 'Model');
+/**
+ * Note Model
+ *
+ * @property User $User
+ */
+class Note extends AppModel {
+
+/**
+ * Display field
+ *
+ * @var string
+ */
+	public $displayField = 'title';
+
+
+	//The Associations below have been created with all possible keys, those that are not needed can be removed
+
+/**
+ * belongsTo associations
+ *
+ * @var array
+ */
+	public $belongsTo = array(
+		'User' => array(
+			'className' => 'User',
+			'foreignKey' => 'user_id',
+			'conditions' => '',
+			'fields' => '',
+			'order' => ''
+		)
+	);
+}
