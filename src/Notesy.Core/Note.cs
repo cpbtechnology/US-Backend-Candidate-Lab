29cad3fe21aaa2d80cfdf72d2b979b427a85b4d1
David Long <longda@gmail.com>
2014-08-08 16:51:02 -0700

    What is a note?  Here's a model to help define it

---
+++ src/Notesy.Core/Note.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Notesy.Core
+{
+    public class Note
+    {
+        public string Contents { get; set; }
+        public bool IsComplete { get; set; }
+    }
+}
