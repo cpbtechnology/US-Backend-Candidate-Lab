eb94a732cd213f5d91565eeaf0233f67ff3e2abd
David Long <longda@gmail.com>
2014-08-08 16:59:53 -0700

    A lot going on here: 1) forgot an id on note model, 2) scafolding out service interface, 3) create/update are basically the same so let's call that Save for now.  4) also adding get and delete calls

--- src/Notesy.Core/Models/Note.cs
+++ src/Notesy.Core/Models/Note.cs
@@ -8,6 +8,7 @@ namespace Notesy.Core.Models
 {
     public class Note
     {
+        public string Id { get; set; }
         public string Contents { get; set; }
         public bool IsComplete { get; set; }
     }
---
+++ src/Notesy.Core/Services/Interfaces/INoteService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Notesy.Core.Models;
+
+namespace Notesy.Core.Services.Interfaces
+{
+    public interface INoteService
+    {
+        Note GetNote(int id);
+        Note SaveNote(Note input);
+        Note DeleteNote(int id);
+    }
+}
