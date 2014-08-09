a487f0e709265b6cce67febdc34f0b479fc82004
David Long <longda@gmail.com>
2014-08-08 19:27:33 -0700

    roughing out the api user models, services, and relationships

---
+++ src/Notesy.Core/Services/Concrete/ApiUserService.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Notesy.Core.Models;
+using Notesy.Core.Services.Interfaces;
+
+namespace Notesy.Core.Services.Concrete
+{
+    public class ApiUserService : IApiUserService
+    {
+
+        public ApiUser GetApiUser(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ApiUser SaveApiUser(ApiUser input)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ApiUser DeleteApiUser(int id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
