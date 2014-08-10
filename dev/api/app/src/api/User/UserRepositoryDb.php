a914e2dec7e7ef2d5a447fb276530150c6ba57d4
Ziad Hilal <ziad.hilal@icloud.com>
2014-08-10 14:09:42 -0400

    Added laravel user repository boilerplate.

--- dev/api/app/src/api/User/UserRepositoryDb.php
+++ dev/api/app/src/api/User/UserRepositoryDb.php
@@ -1,10 +1,92 @@
 <?php namespace api\User;
 
 use api\Library\BaseRepositoryDb;
+use Illuminate\Auth\UserInterface;
+use Illuminate\Auth\Reminders\RemindableInterface;
 use Illuminate\Database\Eloquent\SoftDeletingTrait;
 
-class UserRepositoryDb extends BaseRepositoryDb {
+class UserRepositoryDb extends BaseRepositoryDb implements UserInterface, RemindableInterface {
 
     use SoftDeletingTrait;
+
+    /**
+     * The database table used by the model.
+     *
+     * @var string
+     */
+    protected $table = 'user';
+
+    /**
+     * The attributes excluded from the model's JSON form.
+     *
+     * @var array
+     */
+    protected $hidden = array('password', 'remember_token');
+
+    protected $fillable = [
+        'username',
+        'password',
+    ];
+
+    /**
+     * Get the unique identifier for the user.
+     *
+     * @return mixed
+     */
+    public function getAuthIdentifier()
+    {
+        return $this->getKey();
+    }
+
+    /**
+     * Get the password for the user.
+     *
+     * @return string
+     */
+    public function getAuthPassword()
+    {
+        return $this->password;
+    }
+
+    /**
+     * Get the token value for the "remember me" session.
+     *
+     * @return string
+     */
+    public function getRememberToken()
+    {
+        return $this->remember_token;
+    }
+
+    /**
+     * Set the token value for the "remember me" session.
+     *
+     * @param  string  $value
+     * @return void
+     */
+    public function setRememberToken($value)
+    {
+        $this->remember_token = $value;
+    }
+
+    /**
+     * Get the column name for the "remember me" token.
+     *
+     * @return string
+     */
+    public function getRememberTokenName()
+    {
+        return 'remember_token';
+    }
+
+    /**
+     * Get the e-mail address where password reminders are sent.
+     *
+     * @return string
+     */
+    public function getReminderEmail()
+    {
+        return $this->email;
+    }
 
 }
